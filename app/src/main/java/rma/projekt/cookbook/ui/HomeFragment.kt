98dcb93fbc68f3473fc5de0d48a1678dabc1f6a5
Antonio Cvitkovic <125733906+ancvitko@users.noreply.github.com>
2024-06-15 15:29:55 +0200

    231

--- app/src/main/java/rma/projekt/cookbook/ui/HomeFragment.kt
+++ app/src/main/java/rma/projekt/cookbook/ui/HomeFragment.kt
@@ -1,18 +1,14 @@
 package rma.projekt.cookbook.ui
 
 import android.os.Bundle
-import android.util.Log
 
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 
 import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.tabs.TabLayoutMediator
 
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -21,7 +17,7 @@ import rma.projekt.cookbook.R
 import rma.projekt.cookbook.databinding.FragmentHomeBinding
 import rma.projekt.cookbook.ui.board.TableView
 
-class HomeNewFragment : Fragment() {
+class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
